49f881c26b617f1f8f0ae1050180d8fe8faf5fd2
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2023-11-28 18:01:16 +0800

    mapping RSID on dbSNP using grep

---
+++ preprocess/scripts/02_meQTL_SNP_mapping.py
@@ -0,0 +1,60 @@
+import os
+import pandas as pd
+import numpy as np
+import subprocess
+
+# step1, clean wrong items, fetch rsids
+#'''
+file_path = '../../datasets/raw/Kidney_meQTL.txt'
+output_path = '../../datasets/middlefile/single_meQTL.txt'
+rs_file = '../../datasets/middlefile/meQTL_rsids.txt'
+rsid_list = []
+with open(file_path) as r:
+	with open(output_path,'w+') as w1:
+		line = r.readline()
+		w1.write(line)
+		with open(rs_file,'w+') as w2:
+			line = r.readline()
+			while line:
+				cpg = line.split()[0]
+				rsid = line.split()[1]
+				A1 = line.split()[2]
+				A2 = line.split()[3]
+				if(len(A1) == 1 and len(A2) == 1 and cpg != 'NA' and rsid != 'NA'):
+					w1.write(line)
+					w2.write(rsid + '\n')
+				line = r.readline()
+#'''
+
+# step2 RSID annotation
+#'''
+rs_file = '../../datasets/middlefile/meQTL_rsids.txt'
+output_path = '../../datasets/middlefile/dbSNP_rsid_annotation_raw.txt'
+vcf_file = '/dssg/home/acct-bmelgn/bmelgn-4/dbSNP/00-All.vcf'
+
+rs_list = []
+with open(rs_file) as r:
+	line = r.readline()
+	while line:
+		rs_list.append(line.strip())
+		line = r.readline()
+ext_rs_list = list(set(rs_list))
+print(len(ext_rs_list)) # 2,810,934
+
+# faster: grep command: grep "\<rs1262461\>" /dssg/home/acct-bmelgn/bmelgn-4/dbSNP/00-All.vcf -m 1
+with open(output_path, 'w+') as w:
+	w.write('CHR\tPOS\tRSID\tA1\tA2\n')
+	for rsid in ext_rs_list:
+		try:
+			cmd = 'grep "\<' + rsid + '\>" /dssg/home/acct-bmelgn/bmelgn-4/dbSNP/00-All.vcf -m 1'
+			result = subprocess.check_output(cmd, shell=True) # need decode to string
+			SR = result.decode('utf-8')
+			CHR = SR.split()[0]
+			POS = SR.split()[1]
+			RSID = SR.split()[2]
+			A1 = SR.split()[3]
+			A2 = SR.split()[4]
+			w.write(CHR + '\t' + POS + '\t' + RSID +'\t' + A1 + '\t' + A2 + '\n')
+		except subprocess.CalledProcessError:
+			pass
+#'''

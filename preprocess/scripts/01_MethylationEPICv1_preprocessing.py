3f60286bb34d3879b7ce90b045998db5de3b01c6
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2024-01-25 16:13:17 +0800

    remove null items in MethylationEPIC

--- preprocess/scripts/01_MethylationEPICv1_preprocessing.py
+++ preprocess/scripts/01_MethylationEPICv1_preprocessing.py
@@ -11,14 +11,19 @@ data = pd.read_csv(file_path)
 
 # filter columns
 new_data = data[['IlmnID','CHR_hg38','Start_hg38','End_hg38']]
+new_data.dropna(axis=0, how='any', inplace=True)
+new_data[['Start_hg38']] = new_data[['Start_hg38']].astype(int)
+new_data[['End_hg38']] = new_data[['End_hg38']].astype(int)
 print(new_data.head())
+print(new_data.shape) # (865880, 4)
 
 '''
-       IlmnID CHR_hg38   Start_hg38     End_hg38
-0  cg07881041    chr19    5236004.0    5236006.0
-1  cg18478105    chr20   63216297.0   63216299.0
-2  cg23229610     chr1    6781064.0    6781066.0
-3  cg03513874     chr2  197438741.0  197438743.0
+       IlmnID CHR_hg38  Start_hg38   End_hg38
+0  cg07881041    chr19     5236004    5236006
+1  cg18478105    chr20    63216297   63216299
+2  cg23229610     chr1     6781064    6781066
+3  cg03513874     chr2   197438741  197438743
+4  cg09835024     chrX    24054522   24054524
 '''
 
 # save data

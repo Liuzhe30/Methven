f50827661c822798158ffcab04b60096d59e6979
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2024-05-22 12:17:14 +0800

    train methven

--- preprocess/scripts/09_atac_avg_pooling.py
+++ preprocess/scripts/09_atac_avg_pooling.py
@@ -8,12 +8,12 @@ output_path = '../../datasets/atac_cut/'
 model_cutting = {'small':1,'middle':19,'large':199,'huge':1_999}
 model_size = {'small':1_000,'middle':10_000,'large':100_000,'huge':1_000_000}
 
-for i in range(22):
-    chr = 'chr' + str(i+1)
+for c in range(22):
+    chr = 'chr' + str(c+1)
     for model in model_size.keys():
         data = pd.read_pickle(file_path + chr + '_' + model + '.dataset')
-        for i in range(len(data)):
-            atac = np.array(data['atac_between'][i])
+        for idx in range(len(data)):
+            atac = np.array(data['atac_between'][idx])
 
             new_atac_list = []
             n = model_cutting[model]
@@ -30,7 +30,5 @@ for i in range(22):
             # second 250bp
             new_atac_list.append(np.mean(atac[250+500*n+501+500*n:250+500*n+501+500*n+250]))
 
-            data['atac_between'][i] = new_atac_list
+            data['atac_between'][idx] = new_atac_list
         data.to_pickle(output_path + chr + '_' + model + '.dataset')
-
-

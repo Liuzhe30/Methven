f3b73403fbf63e2225eded67a97531c299c2ad53
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2024-05-31 20:08:40 +0800

    train&evaluate small-model

---
+++ evaluation.py
@@ -0,0 +1,58 @@
+# evaluation
+
+import argparse
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+import os
+import copy
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+
+from model.methven import *
+from src.dataGenerator import dataGenerator
+
+def softmax(vec):
+    """Compute the softmax in a numerically stable way."""
+    vec = vec - np.max(vec)  # softmax(x) = softmax(x+c)
+    exp_x = np.exp(vec)
+    softmax_x = exp_x / np.sum(exp_x)
+    return softmax_x
+
+def evaluate(target_model, data):
+	_, acc = target_model.evaluate(data)
+	print("Restore model, accuracy: {:5.2f}%".format(100*acc))
+
+def predicting(model_size): 
+
+    test_data = pd.read_pickle('datasets/final/dnabert/' + model_size + '_test.dataset')
+    
+    # check model size
+    if(model_size == 'small'):
+        batch_size = 1024
+        model = build_methven_small()
+    elif(model_size == 'large'):
+        batch_size = 512
+        model = build_methven_large()
+    model.summary()
+
+    testGenerator = dataGenerator(test_data, batch_size, model_size)
+
+    model.load_weights('model/weights/' + model_size + '/' + model_size + '_trained_weights.tf').expect_partial()
+    results = model.predict(testGenerator.generate_validation()[0], batch_size=batch_size)
+    label = testGenerator.generate_validation()[1]
+    
+    # save prediction output
+    np.save('model/pred_results/' + model_size + '/' + 'predict.npy', results)
+    np.save('model/pred_results/' + model_size + '/' + 'label.npy', label)
+    
+
+if __name__=='__main__':
+
+    predicting('small')
+    #predicting('middle')
+    #predicting('large')
---
+++ training.py
@@ -0,0 +1,88 @@
+# training
+
+import argparse
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+import os
+import copy
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from sklearn.utils import shuffle
+
+from model.methven import *
+from src.dataGenerator import dataGenerator
+
+# for gpu training
+# os.environ["CUDA_VISIBLE_DEVICES"] = '0' # may needed when DEVICE:0 is occupied, otherwise where will be an error about positional embedding
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model_size', default='small')
+    parser.add_argument('--epochs', default=200, type=int)
+    parser.add_argument('--lr', default=0.0001, type=float,
+                        help="Initial learning rate")
+    parser.add_argument('--lr_decay', default=0.05, type=float,
+                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs") 
+    parser.add_argument('--save_dir', default='model/weights/')
+    parser.add_argument('--debug', action='store_true',
+                        help="Save weights by TensorBoard")
+    args = parser.parse_args()
+    model_size = args.model_size
+
+    path = args.save_dir + model_size + '/'
+    if(not os.path.exists(path)):
+        os.makedirs(path)
+
+    # load datasets
+    train_data = pd.read_pickle('datasets/final/dnabert/' + model_size + '_train.dataset')
+    valid_data = pd.read_pickle('datasets/final/dnabert/' + model_size + '_valid.dataset')
+    test_data = pd.read_pickle('datasets/final/dnabert/' + model_size + '_test.dataset')
+
+    # check model size
+    if(model_size == 'small'):
+        batch_size = 1024
+        model = build_methven_small()
+    elif(model_size == 'large'):
+        batch_size = 512
+        model = build_methven_large()
+
+    model.summary()
+
+    save_dir = args.save_dir
+    
+    # callbacks
+    log = tf.keras.callbacks.CSVLogger(save_dir + model_size + '/log.csv')
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(save_dir + args.model_size + '/' + args.model_size + '_trained_weights.tf', monitor='val_acc', mode='max', #val_categorical_accuracy val_acc
+                                       save_best_only=True, save_weights_only=True, verbose=1)        
+
+    # Train the model and save it
+    model.compile(loss='binary_crossentropy', 
+              optimizer='adam',
+              metrics=['mae', 'acc'])
+    
+    trainGenerator = dataGenerator(train_data, batch_size, model_size)
+    validGenerator = dataGenerator(valid_data, batch_size, model_size)
+    
+    history = model.fit(trainGenerator.generate_batch(), # Tf2 new feature
+          steps_per_epoch = len(train_data)/batch_size,
+          epochs = args.epochs, verbose=1,
+          validation_data = validGenerator.generate_batch(),
+          validation_steps = len(valid_data)/batch_size,
+          callbacks = [log, checkpoint],
+          shuffle = True,
+          workers = 1).history
+
+    print('Trained model saved to \'%s/trained_model.tf\'' % save_dir)

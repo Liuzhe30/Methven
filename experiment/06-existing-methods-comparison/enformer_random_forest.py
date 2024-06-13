11f4c3f8378d48e5797b50de81c9e9facc1a3a61
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2024-06-13 12:13:55 +0800

    enformer-small evaluation

--- experiment/06-existing-methods-comparison/enformer_random_forest.py
+++ experiment/06-existing-methods-comparison/enformer_random_forest.py
@@ -7,61 +7,57 @@ from sklearn.ensemble import RandomForestClassifier
 model_size_list = ['small','large']
 
 final_path = '../../datasets/ten_fold/enformer/'
-output_path = '../../model/prediction_results_tenfold_enformer/'
+output_path = '../../model/pred_results_tenfold_enformer/'
 
 for model_size in model_size_list:
     
     # load & merge datasets
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--fold', default=1, type=int)
-    args = parser.parse_args()
-    fold = str(args.fold)
+    for f in range(10):
+        fold = str(f+1)
 
-    test_data = pd.read_pickle(final_path + model_size + '_split' + fold + '.dataset')
-    merge_data = pd.DataFrame()
-    for i in range(10):
-        if(str(i+1)!= fold):
-            data = pd.read_pickle(final_path + model_size + '_split' + str(i) + '.dataset')
-            merge_data = pd.concat([merge_data,data])
-    train_data = merge_data.reset_index(drop=True)
+        test_data = pd.read_pickle(final_path + model_size + '_split' + fold + '.dataset')
+        merge_data = pd.DataFrame()
+        for i in range(10):
+            if(str(i+1)!= fold):
+                data = pd.read_pickle(final_path + model_size + '_split' + str(i+1) + '.dataset')
+                merge_data = pd.concat([merge_data,data])
+        train_data = merge_data.reset_index(drop=True)
 
-    # training model
-    feature_list = []
-    labels = np.array(train_data['label'].astype("int"))
-    for i in range(train_data.shape[0]):
-        sample_feature = []
-        sample_feature += train_data['result_before'][i].flatten().tolist()
-        sample_feature += train_data['result_after'][i].flatten().tolist()
-        feature_list.append(sample_feature)
-    features = np.array(feature_list)
+        # training model
+        feature_list = []
+        labels = np.array(train_data['label'].astype("int"))
+        for i in range(train_data.shape[0]):
+            sample_feature = []
+            sample_feature += train_data['result_after'][i].flatten().tolist()
+            feature_list.append(sample_feature)
+        features = np.array(feature_list)
 
-    X_train = features
-    Y_train = labels
-    print(X_train.shape)
-    print(Y_train.shape)
+        X_train = features
+        Y_train = labels
+        print(X_train.shape)
+        print(Y_train.shape)
 
-    clf = RandomForestClassifier()
-    clf.fit(X_train,Y_train)
+        clf = RandomForestClassifier()
+        clf.fit(X_train,Y_train)
 
-    # prediction output
-    feature_list = []
-    labels = np.array(test_data['label'].astype("int"))
-    for i in range(test_data.shape[0]):
-        sample_feature = []
-        sample_feature += test_data['result_before'][i].flatten().tolist()
-        sample_feature += test_data['result_after'][i].flatten().tolist()
-        feature_list.append(sample_feature)
-    features = np.array(feature_list)
+        # prediction output
+        feature_list = []
+        labels = np.array(test_data['label'].astype("int"))
+        for i in range(test_data.shape[0]):
+            sample_feature = []
+            sample_feature += test_data['result_after'][i].flatten().tolist()
+            feature_list.append(sample_feature)
+        features = np.array(feature_list)
 
-    X_test = features
-    Y_test = labels
-    print(X_test.shape)
-    print(Y_test.shape)
+        X_test = features
+        Y_test = labels
+        print(X_test.shape)
+        print(Y_test.shape)
 
-    y_score = clf.predict(X_test)                                   
-    y_score_pro = clf.predict_proba(X_test) # (.., 2)
+        y_score = clf.predict(X_test)                                   
+        y_score_pro = clf.predict_proba(X_test) # (.., 2)
 
-    # save results
-    np.save(output_path + model_size + '_split' + fold + '_label.npy',Y_test)
-    np.save(output_path + model_size + '_split' + fold + '_score.npy',y_score)
-    np.save(output_path + model_size + '_split' + fold + '_score_pro.npy',y_score_pro)
+        # save results
+        np.save(output_path + model_size + '_split' + fold + '_label.npy',Y_test)
+        np.save(output_path + model_size + '_split' + fold + '_score.npy',y_score)
+        np.save(output_path + model_size + '_split' + fold + '_score_pro.npy',y_score_pro)

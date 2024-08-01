15641fc7cd74e1d1e306d1ecd2948debf9a56a8f
Zhe Liu <liuzlm1030@sjtu.edu.cn>
2024-08-01 11:13:43 +0800

    finish disease-SNP visualization

--- experiment/08-disease-SNP/07_results_mapping.py
+++ experiment/08-disease-SNP/07_results_mapping.py
@@ -13,6 +13,7 @@ full_t24_path = 'data/final_t24/'
 output_path = 'data/pred_merged/'
 
 model_size = {'small':10_000,'large':100_000}
+model_size = {'small':10_000}
 
 for snp in snp_list:
     for model in model_size.keys():
@@ -44,22 +45,24 @@ for snp in snp_list:
             snp_pos = full_t0_data[full_t0_data['CpG']==item]['SNP_POS'].values[0]
             t0 = full_t0_data[full_t0_data['CpG']==item]['t0'].values[0]
             t24 = full_t24_data[full_t24_data['CpG']==item]['t24'].values[0]
+            delta = t24 - t0
             final_df = final_df._append({'CpG':cpg,'SNP':snp,'Ref':ref,'Alt':alt,'CHR':chr,'CpG_POS':cpg_pos,
-                                            'SNP_POS':snp_pos,'t0':t0,'t24':t24},ignore_index=True)
+                                            'SNP_POS':snp_pos,'t0':t0,'t24':t24,'delta':delta},ignore_index=True)
         print(final_df.head())
         final_df.to_csv(output_path + snp + '_' + model + '.csv')
 
 '''
-          CpG       SNP Ref Alt CHR  CpG_POS  SNP_POS        t0       t24
-0  cg15084823  rs947474   G   A  10  6392160  6390450  5.808340  6.500612
-1  cg18100746  rs947474   G   A  10  6390958  6390450  0.073516 -0.612236
-2  cg24277140  rs947474   G   A  10  6392062  6390450  4.486728  4.792028
-3  cg10718100  rs947474   G   A  10  6392069  6390450  4.904702  5.263696
-4  cg22423996  rs947474   G   A  10  6392132  6390450  5.301204  6.127559
-          CpG        SNP Ref Alt CHR  CpG_POS  SNP_POS        t0       t24
-0  cg17489908  rs3824660   C   G  10  8101566  8104722 -0.893736 -0.806910
-1  cg01166071  rs3824660   C   G  10  8095687  8104722  2.413714  0.571770
-2  cg15187550  rs3824660   C   G  10  8096370  8104722 -3.281850 -2.574403
-3  cg11018337  rs3824660   C   G  10  8095495  8104722  1.451097  0.640844
-4  cg17566118  rs3824660   C   G  10  8095797  8104722  1.147711 -0.970443
+          CpG        SNP Ref Alt CHR   CpG_POS   SNP_POS        t0       t24  \
+0  cg00660737  rs3218251   T   A  22  37546043  37545505 -2.064718 -1.780150
+1  cg07278285  rs3218251   T   A  22  37554775  37545505 -0.111967  0.490153
+2  cg11619069  rs3218251   T   A  22  37543486  37545505  1.022392  0.692455
+3  cg18060391  rs3218251   T   A  22  37536870  37545505 -3.512755 -2.801362
+4  cg00154335  rs3218251   T   A  22  37554221  37545505  1.773641  0.025784
+
+      delta
+0  0.284569
+1  0.602120
+2 -0.329937
+3  0.711393
+4 -1.747857
 '''
